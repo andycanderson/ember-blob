e3bf0b0f72ef19185ad8a37912f800d7c52cc01a
andycanderson <chen.a.andy@gmail.com>
2015-10-01 10:32:45 -0700

    rename this repo

--- app/router.js
+++ app/router.js
@@ -5,7 +5,8 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
+  this.route('about');
+  this.route('projects');
+  this.route('contact');
 });
-
-export default Router;
